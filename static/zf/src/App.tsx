51e4fa823fa43ce54a1f8e5c37836c4d037a33ba
yangzhixiao <yangzhixiao1989@foxmail.com>
2019-07-17 10:24:44 +0800

    front

---
+++ static/zf/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ZFList from './ZFList';
+import 'antd/dist/antd.css';
+
+const App: React.FC = () => {
+  return (
+    <ZFList />
+  );
+}
+
+export default App;
---
+++ static/zf/src/ZFList.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import { Table, Button } from 'antd'
+import data from './mock.json'
+
+const DOMAIN = 'http://localhost:5000'
+
+export default class ZFList extends React.Component<any, any> {
+
+  public state = {
+    data: [],
+    loading: false,
+  }
+
+  public componentDidMount() {
+    // this.fetchData()
+    this.setState({ data: data })
+  }
+
+  public render() {
+    const { data, loading } = this.state
+    return (
+      <Table
+        style={{ padding: '15px' }}
+        rowKey='id'
+        loading={loading}
+        dataSource={data}
+        pagination={{ position: 'bottom', pageSize: 8, }}
+        columns={[
+          { title: '图片', dataIndex: 'imgs', 
+            render: (t: any, r: any, i: number) => {
+              return (
+                <div>
+                  <div style={{lineHeight: '20px', marginBottom: '5px', marginLeft: '10px'}}>
+                    {`${r.updatetime} ${r.title}`}
+                  </div>
+                  {r.imgs && r.imgs.split(',').map((i: string) => 
+                    <a
+                      key={i}
+                      href={DOMAIN + '/static/images/' + i}
+                      target='_blank'
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        style={{margin: '5px'}}
+                        alt={i}
+                        src={DOMAIN + '/static/images/' + i.replace('.jpg', '_thumb.jpg')} 
+                      />
+                    </a>
+                  )}
+                </div>
+              )
+            }
+          },
+          { title: '操作', dataIndex: 'id', width: 150,
+            render: (t: any, r: any, i: number) => {
+              return (
+                <Button
+                  type='primary'
+                  onClick={() => this.handleDownloadClick(r.id)}
+                >
+                  下载图片
+                </Button>
+              )
+            }
+          },
+        ]}
+      />
+    )
+  }
+
+  public handleDownloadClick = (id: string) => {
+    window.open(DOMAIN + '/download/' + id, '_blank')
+  }
+
+  public fetchData = async () => {
+    this.setState({ loading: true })
+    const ret = await fetch(DOMAIN + '/api/list')
+    this.setState({ data: ret, loading: false })
+  }
+}
